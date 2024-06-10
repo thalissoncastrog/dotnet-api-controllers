339438ca1ceea6276a44d31a0a4c4fd2394a39ec
Thalisson Castro <adaothalisson@hotmail.com>
2024-06-10 11:33:11 -0300

    Created a DTO approach

--- src/TodoApi/Models/TodoItemDTO.cs
+++ src/TodoApi/Models/TodoItemDTO.cs
@@ -1,9 +1,10 @@
+using System.Drawing;
+
 namespace TodoApi.Models;
 
-public class TodoItem
+public class TodoItemDTO
 {
     public long Id { get; set; }
     public string? Name { get; set; }
     public bool IsComplete { get; set; }
-    public string? Secret { get; set; }
 }
